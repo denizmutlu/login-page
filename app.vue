34e7dafe1e5fd308ed3241d90c33087f0921da8e
denizmutlu <56771309+denizmutlu@users.noreply.github.com>
2024-03-21 23:37:18 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app theme="dark">
     <v-app-bar title="Application bar" class="text-center" />
-    <v-main class="d-flex align-center justify-center">
+    <v-main class="d-flex justify-center align-center">
       Main Content
     </v-main>
     <v-footer app class="d-flex justify-center">
