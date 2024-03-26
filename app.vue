bbee14dd135b265875116ce4bbfff0a14dfffc39
denizmutlu <56771309+denizmutlu@users.noreply.github.com>
2024-03-26 18:16:24 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,11 +1,27 @@
 <template>
   <v-app theme="dark">
-    <v-app-bar title="Application bar" class="text-center" />
+    <v-app-bar :title="title" class="text-center" />
     <v-main class="d-flex justify-center align-center">
-      Main Content
+      <v-row class="ma-0">
+        <v-col cols"auto" class="text-h4">
+          {{ signin }}
+        </v-col>
+        <v-col cols"12" class="pa-0" />
+        <v-col>
+          <v-text-field rounded variant="solo-inverted"  />
+        </v-col>
+      </v-row>
     </v-main>
     <v-footer app class="d-flex justify-center">
       {{ new Date().getFullYear() }}
     </v-footer>
   </v-app>
 </template>
+<script setup>
+  const language = {
+    en: {
+      title: 'My Title',
+      signin: 'Sign in'
+    }
+  }
+</script>
